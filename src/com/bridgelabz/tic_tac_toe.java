d02aff7d995383e21b1fb4c8447beda224535f47
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-05-30 14:43:27 +0530

    uc1

--- src/com/bridgelabz/tic_tac_toe.java
+++ src/com/bridgelabz/tic_tac_toe.java
@@ -2,7 +2,18 @@ package com.bridgelabz;
 
 public class tic_tac_toe {
 
+    void uc1(char [] ch){
+        for(int i=1;i<10;i++){
+            ch[i]=' ';
+        }
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to tic_tac_toe");
+        System.out.println("Welcome to Tic Tac Toe program");
+
+        char [] ch = new char[10];
+        tic_tac_toe t1 = new tic_tac_toe();
+        t1.uc1(ch);
     }
 }
